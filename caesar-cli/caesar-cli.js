c7e33d82309a62985fc423c8d5d15625666054ef
Marat Maksumov <maxumov@gmail.com>
2020-10-03 18:38:56 +0300

    feat: implement basic CLI options parsing
    import yargs npm package
    add parsing rules for the required --action parameter
    add parsing rules for the required --shift parameter
    add parsing rules for the optional --input parameter
    add parsing rules for the optional --output parameter
    define the --version, help & --usage arguments
    set default locale to en
    add basic parse error catching
    add copyright output :D

---
+++ caesar-cli/caesar-cli.js
@@ -0,0 +1,65 @@
+const yargs = require("yargs");
+const pjson = require("./package.json");
+const path = require("path");
+
+yargs
+  .usage("Usage: $0 options")
+  .example([
+    [
+      "$0 --shift=7 --action=encode",
+      "Encode data from stdin with shift 7 and print result to stdout",
+    ],
+    [
+      "$0 -s 2 -a decode -i topsecret.txt",
+      "Decode topsecret.txt with shift 2 and print result to stdout",
+    ],
+  ])
+  .version(pjson.version)
+  .option("shift", {
+    alias: "s",
+    demandOption: true,
+    describe: "Set the shift for decode/encode data",
+    requiresArg: true,
+    type: "number",
+  })
+  .option("action", {
+    alias: "a",
+    choices: ["encode", "decode"],
+    demandOption: true,
+    describe: "Specify what action you want to perform",
+    requiresArg: true,
+  })
+  .option("input", {
+    alias: "i",
+    demandOption: false,
+    describe: "Specify the file where to get the data from",
+    normalize: true,
+    requiresArg: true,
+    type: "string",
+  })
+  .option("output", {
+    alias: "o",
+    demandOption: false,
+    describe: "Specify the file to save the data to",
+    normalize: true,
+    requiresArg: true,
+    type: "string",
+  })
+  .help("help")
+  .locale("en")
+  .alias("help", "h")
+  .wrap(yargs.terminalWidth())
+  .epilog("(c) 2020 Marat Maksumov")
+  .scriptName(`node ${path.basename(__filename)}`)
+  .parse(process.argv, {}, (err, argv, output) => {
+    console.log("argv :>> ", argv);
+    console.log("------------------------ ");
+    if (err !== null) {
+      console.error("Ошибка! :>> ");
+      console.log("argv :>> ", err);
+      console.log("------------------------ ");
+      process.exitCode = -1;
+    }
+  });
+
+console.dir(yargs.argv);
