6775be31206eb71ee17fa7eb11ad189362988b57
Marat Maksumov <maksumov@users.noreply.github.com>
2020-10-05 23:21:01 +0300

    feat: colorize error messages
    add chalk package dependency
    add color accent to error messages

--- caesar-cli/utils/error-handler.js
+++ caesar-cli/utils/error-handler.js
@@ -1,5 +1,7 @@
+const chalk = require("chalk");
+
 const errorHandler = (context, err) => {
-  console.error(`[${context}]: ${err}`);
+  console.error(chalk.red(`[${chalk.bold(context)}]: ${err}`));
   process.exit(-1);
 };
 
